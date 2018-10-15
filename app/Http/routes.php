7ce3b867bebcda1950425930d619cb4671f6d115
Yismen Jorge <yismen.jorge@gmail.com>
2018-10-15 15:53:18 -0300

    Added route or Empleados
    Signed-off-by: Yismen Jorge <yismen.jorge@gmail.com>

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,5 +6,8 @@ Router::get('', 'PagesController@home');
 Router::get('home', 'PagesController@home');
 Router::get('about', 'PagesController@about');
 Router::get('clients', 'ClientsController@index');
+Router::get('incentives', 'IncentiveController@index');
 Router::post('clients', 'ClientsController@store');
 Router::get('about/culture', 'PagesController@culture');
+
+Router::get('empleados', 'EmpleadosController@index');
